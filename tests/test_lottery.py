3d971ebabf4cec9c21f7694bf043098623f889b0
Sakib Rahman <srskb15@gmail.com>
2022-05-17 00:37:46 +0600

    SA | test files added

---
+++ tests/test_lottery.py
@@ -0,0 +1,12 @@
+from brownie import Lottery, accounts, network, config
+from web3 import Web3
+
+
+def test_entrance_fee():
+    account = accounts[0]
+    lottery = Lottery.deploy(
+        config["networks"][network.show_active()]["eth_usd_price_feed"],
+        {"from": account},
+    )
+    assert lottery.getEntranceFee() > Web3.toWei(0.018, "ether")
+    assert lottery.getEntranceFee() < Web3.toWei(0.024, "ether")
